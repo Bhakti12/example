ecc5f20d213c73931e35fed33bc2b0e3f8342a3b
Bhakti12 <sanghanibhakti922@gmail.com>
2023-09-15 12:01:21 +0530

    created functional component and arrow function component and export and import functions

---
+++ src/components/function_component_example.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+//simple function component
+function Greet() {
+    return <h1>This is Functional Component</h1>
+}
+
+//arrow function component
+const ArrowExample = () => <h1>This is Arrow Function</h1> 
+
+export {Greet , ArrowExample}
